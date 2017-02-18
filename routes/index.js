26e4bf28b13d202622c8d7d4f3e1feefd07c446f
chantebryana <chantebryana@gmail.com>
2017-02-18 12:45:47 -0600

    committing some changes Jim and I made, including some javascripting within a web page (along with several errors that were made along the way)

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+
 /* GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -8,16 +10,19 @@ router.get('/', function(req, res, next) {
 */
 
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Home' });
+  res.render('pages/index', { title: 'Index' });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
+  fs.readdir('/home/ruby/', (err, files) => {
+    res.render('pages/about', { title: 'About', files: files });
+  });
 });
 
+/*
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
-
+*/
 
 module.exports = router;
